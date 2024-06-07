dabb5c888200a3b7adc510df405cd07d13254e76
MrcSanto <marco.antonio@santolin.com.br>
2024-06-07 18:13:42 -0300

    Apredendo usar C#

---
+++ Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Library;
+
+namespace OOPproject
+{
+    class Library
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Book livro1 = new Book("O Senhor dos Anéis", "J.R.R. Tolkien", "HarperCollins", 1216, 100);
+
+            livro1.showInfo();
+
+            Book livro2 = new Book();
+            livro2.showInfo();
+        }
+    }
+}
+
